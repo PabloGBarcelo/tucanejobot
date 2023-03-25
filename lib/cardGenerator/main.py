dc8cd67a9fd30d3b2b2703bd3231173cca166676
Pablo <pablo@gutierrezbarcelo.com>
2023-03-25 15:18:31 +0100

    feat: transform png to jpeg

--- lib/cardGenerator/main.py
+++ lib/cardGenerator/main.py
@@ -3,6 +3,8 @@ import os, random
 from pyquery import PyQuery as pq
 from html2image import Html2Image
 from datetime import datetime
+from uuid import uuid4
+import cv2
 
 hti = Html2Image(
     browser_executable="./chrome/App/Chrome-bin/chrome.exe",
@@ -13,37 +15,42 @@ hti = Html2Image(
 def createCard(html_str):
     now = datetime.now()
     nameFile = "photo" + str(now.strftime("%Y-%m-%d-%H%M%S")) + ".png"
-    hti.screenshot(
+    result = hti.screenshot(
         html_str=html_str,
         css_file="templates/style.css",
         save_as=nameFile,
     )
-    return nameFile # name of photo to upload
+    return nameFile  # name of photo to upload
 
 
 def generateCard(data, name, rootDir):
+    # Select font color
+    fontColor = random.choice(["white","black"])
+
     # load random image
-    fontColor = random.choice(os.listdir(".\\templates\\cards\\"))
-    print(fontColor)
     imagePath = (
         str(rootDir)
-        + f"\\templates\\cards\\{fontColor}\\" 
+        + f"\\templates\\cards\\{fontColor}\\"
         + random.choice(os.listdir(f".\\templates\\cards\\{fontColor}\\"))
     )
-    
     # calculate average
-    average = str(int(sum(data.values()) / len(data)))
+    dataForAverage = {
+        k: v
+        for k, v in data.items()
+        if k in ["puta", "calvo", "autonomo", "pene", "facha", "fairy"]
+    }
+    average = str(int(sum(dataForAverage.values()) / len(dataForAverage)))
     # load html
     htmlString = pq(open("./templates/baseCard.html").read())
     # set font color
-    htmlString("#container").attr("class",fontColor)
+    htmlString("#container").attr("class", fontColor)
     # Fill name
     htmlString(".cardContent")(".name")(".din-font").append(name)
     # Fill average
     htmlString(".cardContent")(".average")(".din-font").append(average)
     # Fill image path
     htmlString("#cardModel").attr("src", imagePath)
-    data['auton'] = data['autonomo']
+    data["auton"] = data["autonomo"]
     for value in data:
         # Fill text
         htmlString(".cardStaticContent")("." + value.lower())(".din-font").append(
@@ -56,17 +63,44 @@ def generateCard(data, name, rootDir):
 
     # create file in temp folder
     nameFile = createCard(str(htmlString))
-    return nameFile
+    # transform to jpg
+    image = cv2.imread(nameFile)
+    nameFileJPG = nameFile.replace(".png", ".jpg")
+    cv2.imwrite(nameFileJPG, image, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
+    return nameFileJPG
 
 
-def constructResultPhoto(thumb_url, image_url):    
-    # return inlinequeryResultPhoto
+def constructResultPhoto(thumb_url, image_url, width, height, completeResume):
     return InlineQueryResultPhoto(
-        id="Cromo",
+        id=uuid4(),
         title="Crea una Medalla con tus características",
         description="¡Muestra con orgullo tu mierda de característicasS!",
+        caption=completeResume,
         photo_url=image_url,
-        photo_width=800,
-        photo_height=1150,
         thumb_url=thumb_url,
+        photo_width=width,
+        photo_height=height,
+    )
+
+
+def constructOptionToCallback(thumb_url, image_url, width, height, completeResume):
+    return InlineQueryResultPhoto(
+        id="Medalla",
+        title="Crea una Medalla con tus características",
+        description="¡Muestra con orgullo tu mierda de característicasS!",
+        caption=completeResume,
+        photo_url=image_url,
+        thumb_url=thumb_url,
+        photo_width=width,
+        photo_height=height,
+        reply_markup={
+            "inline_keyboard": [
+                [
+                    {
+                        "text": "⌛",
+                        "callback_data": "doNothing",
+                    }
+                ]
+            ]
+        },
     )
