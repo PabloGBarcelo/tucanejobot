2c9c653281d02a24053f1c61865885a010addc36
Pablo <pablo@gutierrezbarcelo.com>
2023-03-26 18:23:32 +0200

    fix: compatibility Linux and Windows

--- definitions.py
+++ definitions.py
@@ -1,6 +1,7 @@
-import os, json
+import os, json, platform
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+SLASH = "/" if platform.system() == "Linux" else "\\"
 
 f = open("./assets/options.json", encoding="utf-8")
 options = json.load(f)
